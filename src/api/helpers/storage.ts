891df7bdd3bad2b51986e33eb65cb580966b3790
SamLopC <sammy.lopz18@gmail.com>
2025-02-19 16:12:37 -0700

    public url vs 1 hour

--- src/api/helpers/storage.ts
+++ src/api/helpers/storage.ts
@@ -1,13 +1,23 @@
-// helpers/storage.ts
+
 import admin from '../config/firebase';
 
 export async function getDownloadURL(filePath: string): Promise<string> {
   const bucket = admin.storage().bucket();
   const file = bucket.file(filePath);
-  const expires = Date.now() + 60 * 60 * 1000; // URL valid for 1 hour
+  const expires = Date.now() + 60 * 60 * 1000; //1 hora 
   const [url] = await file.getSignedUrl({
     action: 'read',
     expires,
   });
   return url;
 }
+
+
+
+//PUBLIC URL
+// export async function getDownloadURL(filePath: string): Promise<string> {
+//   const bucket = admin.storage().bucket();
+//   const file = bucket.file(filePath);
+//   await file.makePublic();
+//   return `https://storage.googleapis.com/${bucket.name}/${filePath}`;
+// }
